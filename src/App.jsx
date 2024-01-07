6e92e37116fe903767b037b041cfb267f4efc3e3
VictorIdowu <idsonvictor@gmail.com>
2024-01-07 17:53:39 +0100

    onBoarding state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Quiz from "./components/Quiz.jsx";
+import { Landing } from "./components/Landing.jsx";
 
 const App = () => {
+  const [start, setStart] = useState(false);
   return (
     <>
       <Header />
       <main>
-        <Quiz />
+        {!start && <Landing setStart={setStart} />}
+        {start && <Quiz />}
       </main>
     </>
   );
