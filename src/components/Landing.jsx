d1157b109c1f60fdf4cde065ea6f0f794bac8e0a
VictorIdowu <idsonvictor@gmail.com>
2024-01-09 19:22:29 +0100

    ...

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Landing = ({ setStart }) => {
   return (
-    <div className="max-w-[50rem] m-auto p-8 bg-gradient-to-b from-[#338375] from-0% to-[#072a24] to-100% rounded-lg shadow-sm text-center">
+    <section className="max-w-[50rem] m-auto p-8 bg-gradient-to-b from-[#338375] from-0% to-[#072a24] to-100% rounded-lg shadow-sm text-center">
       <p className="text-xl">Are you ready to take this quiz?</p>
       <button
         className="mt-8 text-[#3f7a70] font-Condensed text-[0.9rem] py-4 px-8 bg-gradient-to-b from-[#81f1fb] from-0% to-[#73d2f8] to-100% hover:from-[#3f7a70] hover:to-[#55b9a9] hover:text-white cursor-pointer transition-all duration-700 ease-in-out rounded-3xl text-xl"
@@ -10,6 +10,6 @@ export const Landing = ({ setStart }) => {
       >
         Get started
       </button>
-    </div>
+    </section>
   );
 };
